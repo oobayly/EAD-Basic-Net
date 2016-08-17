d5d74c8ae4eda3256a2749f16d4b352540f222fe
oobayly <oobayly@yahoo.com>
2016-08-17 16:46:13 +0100

    Add generic request wrapper when posting literals

---
+++ EAD-Basic-Net/Request.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace eu.bayly.EADBasicNet {
+  /// <summary>
+  /// A wrapper to allow literals to be posted.
+  /// </summary>
+  public class Request<T> {
+    /// <summary>
+    /// Gets or sets the request's value.
+    /// </summary>
+    public T Value { get; set; }
+  }
+}
