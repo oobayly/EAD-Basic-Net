b8a024a48b01adaa022441b1b971b73a1698ceb8
oobayly <oobayly@yahoo.com>
2016-08-17 16:37:43 +0100

    Move CORS code in global.asax as some webhosts block it in web.config

--- EAD-Basic-Net/Global.asax.cs
+++ EAD-Basic-Net/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 
@@ -17,6 +18,20 @@ namespace eu.bayly.EADBasicNet {
     }
 
     protected void Application_BeginRequest(object sender, EventArgs e) {
+      // Some web hosts disable web.config options - namely system.webServer/httpProtocol/
+      // So create the CORS headers here instead
+      if (HttpContext.Current.Request.AppRelativeCurrentExecutionFilePath.StartsWith("~/api")) {
+        HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+
+        if (HttpContext.Current.Request.HttpMethod == "OPTIONS") {
+          //These headers are handling the "pre-flight" OPTIONS call sent by the browser
+          HttpContext.Current.Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+          HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+          HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST,OPTIONS");
+          HttpContext.Current.Response.AddHeader("Access-Control-Expose-Headers", "Content-Disposition, Last-Modified");
+          HttpContext.Current.Response.End();
+        }
+      }
     }
 
     protected void Application_AuthenticateRequest(object sender, EventArgs e) {
