773e63edbefc4e87cfb9b478c8c00432aa2a9826
oobayly <oobayly@yahoo.com>
2016-08-18 22:54:33 +0100

    Suppress null values from being serialized.

--- EAD-Basic-Net/WebApiConfig.cs
+++ EAD-Basic-Net/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -21,6 +22,8 @@ namespace eu.bayly.EADBasicNet {
         name: "ActionApi",
         routeTemplate: "api/{controller}/{action}"
         );
+
+      config.Formatters.JsonFormatter.SerializerSettings = new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore };
     }
   }
 }
