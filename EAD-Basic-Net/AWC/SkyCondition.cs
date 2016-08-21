d5ce264f4870b8df4e586c86fd920c6bba7a568a
oobayly <oobayly@yahoo.com>
2016-08-21 01:03:24 +0100

    Add cloud type

--- EAD-Basic-Net/AWC/SkyCondition.cs
+++ EAD-Basic-Net/AWC/SkyCondition.cs
@@ -5,12 +5,12 @@ using System.Xml.Serialization;
 
 namespace eu.bayly.EADBasicNet.AWC {
   /// <summary>
-  /// Represents the Sky Condition value for a METAR/TAF report.
+  /// Represents the sky condition value for a METAR/TAF report.
   /// </summary>
   [XmlRoot("sky_condition")]
   public class SkyCondition {
     /// <summary>
-    /// Gets or sets the type of Cover
+    /// Gets or sets the type of Cover.
     /// </summary>
     [JsonConverter(typeof(StringEnumConverter))]
     [XmlAttribute("sky_cover")]
@@ -20,6 +20,12 @@ namespace eu.bayly.EADBasicNet.AWC {
     /// Gets or sets the cloud base (Above ground level in Feet).
     /// </summary>
     [XmlAttribute("cloud_base_ft_agl")]
-    public string CloudBase { get; set; }
+    public string Base { get; set; }
+
+    /// <summary>
+    /// Gets or sets the type of cloud.
+    /// </summary>
+    [XmlElement("cloud_type")]
+    public string Type { get; set; }
   }
 }
